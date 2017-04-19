baude/atomic

commit c134ee581b0e08fd0dcf93d1d2e55189dbeec7d1

Minor fix to install logic
Add check for image existing in local backends, which allows for
'atomic.type=system' labelled containers to be installed without
an extra remote check.

Signed-off-by: Yu Qi Zhang <jerzhang@redhat.com>

Closes: #954
Approved by: rhatdan

Atomic/install.py
@@ -109,11 +109,9 @@ class Install(Atomic):
         elif OSTREE_PRESENT and self.args.setvalues:
             raise ValueError("--set is valid only when used with --system or --user")
 
-        # Assumed backend now is docker
-        be = be_utils.get_backend_from_string('docker')
+        # Check if image exists in any local backends
+        be, img_obj = be_utils.get_backend_and_image_obj(self.image, str_preferred_backend=self.args.storage or storage, required=True if self.args.storage else False)
 
-        # If the image is already present,
-        img_obj = be.has_image(self.image)
         if img_obj is None:
             remote_image_obj = be.make_remote_image(self.args.image)
             # We found an atomic.type of system, therefore install it onto the ostree
@@ -123,6 +121,10 @@ class Install(Atomic):
                 return self.syscontainers.install(self.image, self.name)
             be.pull_image(self.args.image, remote_image_obj, debug=self.args.debug)
             img_obj = be.has_image(self.image)
+
+        if img_obj.is_system_type:
+            return self.syscontainers.install(self.image, self.name)
+
         install_args = img_obj.get_label('INSTALL')
         if not install_args:
             return 0

commit d6a5e486f78b92e561e5ae3880b87eb680d9c46e

Atomic/install.py: Fix what c134ee5 broke
The commit c134ee5 broke the ability for atomic install to pull an image
if it isnt present.

Also, changed the flow in install.py to use more of the backend
objects when determining the backend, the image object, and the
remote image objects.

Atomic/install.py
@@ -104,26 +104,34 @@ class Install(Atomic):
             if not util.is_user_mode():
                 raise ValueError("--user does not work for privileged user")
             return self.syscontainers.install_user_container(self.image, self.name)
-        elif self.system or storage == 'ostree':
+        if self.ostree_uri(self.image):
             return self.syscontainers.install(self.image, self.name)
-        elif OSTREE_PRESENT and self.args.setvalues:
-            raise ValueError("--set is valid only when used with --system or --user")
-
-        # Check if image exists in any local backends
-        be, img_obj = be_utils.get_backend_and_image_obj(self.image, str_preferred_backend=self.args.storage or storage, required=True if self.args.storage else False)
+        # Check if image exists
+        str_backend = 'ostree' if self.args.system else self.args.storage or storage
+        be = be_utils.get_backend_from_string(str_backend)
+        img_obj = be.has_image(self.args.image)
+        if img_obj and img_obj.is_system_type:
+            be = be_utils.get_backend_from_string('ostree')
 
         if img_obj is None:
+            # Unable to find the image locally, look remotely
             remote_image_obj = be.make_remote_image(self.args.image)
             # We found an atomic.type of system, therefore install it onto the ostree
             # backend
             if remote_image_obj.is_system_type and not storage_set:
                 be_utils.message_backend_change('docker', 'ostree')
-                return self.syscontainers.install(self.image, self.name)
+                be = be_utils.get_backend_from_string('ostree')
             be.pull_image(self.args.image, remote_image_obj, debug=self.args.debug)
             img_obj = be.has_image(self.image)
 
-        if img_obj.is_system_type:
-            return self.syscontainers.install(self.image, self.name)
+        if be.backend is not 'docker':
+            if OSTREE_PRESENT and self.args.setvalues and not self.user and not self.system:
+                raise ValueError("--set is valid only when used with --system or --user")
+
+            # We need to fix this long term and get ostree
+            # using the backend approach vs the atomic args
+            be.syscontainers.set_args(self.args)
+            return be.install(self.image, self.name)
 
         install_args = img_obj.get_label('INSTALL')
         if not install_args:
@@ -147,4 +155,11 @@ class Install(Atomic):
     @staticmethod
     def print_install():
         return "%s %s %s" % (util.default_docker(), " ".join(INSTALL_ARGS), "/usr/bin/INSTALLCMD")
+
+    @staticmethod
+    def ostree_uri(image_name):
+        for i in ['dockertar:', 'ostree:', 'docker:']:
+            if image_name.startswith(i):
+                return True
+        return False
 

atomic_dbus.py
@@ -373,7 +373,7 @@ class atomic_dbus(slip.dbus.service.Object):
     # The Install method will install the specified image
     @slip.dbus.polkit.require_auth("org.atomic.readwrite")
     @dbus.service.method("org.atomic", in_signature='ssbbsbas', out_signature='i')
-    def Install(self, image, name='', system=False, remote=False, storage='', user=False, setvalues=''):
+    def Install(self, image, name, system, remote, storage, user, setvalues):
         if not setvalues:
             setvalues = []
         assert(isinstance(setvalues, list))
@@ -417,11 +417,11 @@ class atomic_dbus(slip.dbus.service.Object):
     @slip.dbus.polkit.require_auth("org.atomic.readwrite")
     # Return a 0 or 1 for success.  Errors result in exceptions.
     @dbus.service.method("org.atomic", in_signature='ssbbbas', out_signature='i')
-    def Run(self, image, name='', spc=False, detach=False, ignore=False, command=''):
+    def Run(self, image, name, spc, detach, ignore, command):
         r = Run()
         args = self.Args()
         args.image = image
-        args.name = name if name is not '' else None
+        args.name = name
         args.spc = spc
         args.detach = detach
         args.command = command if command is not '' else []

tests/integration/test_dbus.py
@@ -105,11 +105,10 @@ class TestDBus():
 
     @integration_test
     def test_run(self):
-        self.dbus_object.Run('atomic-test-3', 'atomic-dbus-3', False, False, True)
+        self.dbus_object.Run('atomic-test-3', 'atomic-dbus-3', False, False, True, '')
         self.cid = TestDBus.run_cmd('docker ps -aq -l').decode('utf-8').rstrip()
         TestDBus.add_cleanup_cmd('docker rm {}'.format(self.cid))
         container_inspect = json.loads(TestDBus.run_cmd('docker inspect {}'.format(self.cid)).decode('utf-8'))[0]
-        print(container_inspect)
         assert(container_inspect['Name'] == '/atomic-dbus-3')
 
     @integration_test
@@ -132,8 +131,7 @@ class TestDBus():
         t_cid = TestDBus.run_cmd('docker ps -aq -l').decode('utf-8').rstrip()
         TestDBus.run_cmd('docker commit {} dbus-test-3'.format(t_cid))
         TestDBus.run_cmd('docker rm {}'.format(t_cid))
-
-        results = self.dbus_object.Install('dbus-test-3', name='atomic-dbus-3')
+        results = self.dbus_object.Install('dbus-test-3', 'dbus-test-3', False, False, 'docker', False, '')
         self.cid = TestDBus.run_cmd('docker ps -aq -l').decode('utf-8').rstrip()
         TestDBus.add_cleanup_cmd('docker rm {}'.format(self.cid))
         TestDBus.add_cleanup_cmd('docker rmi atomic-dbus-3')